827cd89274fb2e68b349fc9da3578078fb5ee46c
Phantom-Farmer <joeljdahl@gmail.com>
2022-08-22 18:47:20 -0500

    most basic api calls

---
+++ components/api/dreamCardData.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { clientCredentials } from '../../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+const getDreamCardsByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/dreamCards.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((error) => reject(error));
+});
+
+const createDreamCards = (dcObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/users.json?`, dcObj)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/dreamCards/${response.data.name}.json`, payload).then(() => {
+        getDreamCardsByUid(dcObj.uid).then((dcArray) => resolve(dcArray));
+      });
+    }).catch((error) => reject(error));
+});
+
+const getSingleDreamCard = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/dreamCards/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const deleteSingleDreamCard = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/dreamCards/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const updateDreamCard = (dcObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/dreamCards/${dcObj.firebaseKey}.json`, dcObj)
+    .then(() => getDreamCardsByUid(dcObj.uid)).then(resolve)
+    .catch(reject);
+});
+
+export {
+  getDreamCardsByUid,
+  createDreamCards,
+  getSingleDreamCard,
+  deleteSingleDreamCard,
+  updateDreamCard,
+
+};
