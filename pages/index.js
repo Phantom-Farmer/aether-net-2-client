45409cb79a771f8e7af7e44af89246cd9d302d71
Phantom-Farmer <joeljdahl@gmail.com>
2022-08-25 21:31:57 -0500

    yadda yadda why wont my sleepcards render

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
-function Home() {
+/* eslint-disable react-hooks/exhaustive-deps */
+import { React, useEffect, useState } from 'react';
+import { getSleepCardsByUid } from '../api/sleepCardData';
+import SleepCard from '../components/SleepCard';
+import { useAuth } from '../utils/context/authContext';
+
+export default function Home() {
+  const [sleepCards, setSleepCards] = useState([]);
+  const { user } = useAuth();
+  const getAllSleepCards = () => {
+    getSleepCardsByUid(user.uid).then(setSleepCards);
+  };
+
+  useEffect(() => {
+    getAllSleepCards(user.uid);
+  }, [user.uid]);
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    />
+    <div className="d-flex flex-wrap">
+      {sleepCards.map((scObj) => (
+        <SleepCard key={scObj.firebaseKey} scObj={scObj} onUpdate={getAllSleepCards} />
+      ))}
+    </div>
   );
 }
-
-export default Home;
