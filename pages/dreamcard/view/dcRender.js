87f6fa994b9acb8a9de70f1e93853c48434455e4
Phantom-Farmer <joeljdahl@gmail.com>
2022-08-29 23:12:11 -0500

    "okie dokie"

---
+++ pages/dreamcard/view/dcRender.js
@@ -0,0 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { React, useEffect, useState } from 'react';
+import { getDreamCardsByUid } from '../../../api/dreamCardData';
+import DreamCard from '../../../components/DreamCard';
+import { useAuth } from '../../../utils/context/authContext';
+
+export default function DisplayDc() {
+  const [dreamCards, setDreamCards] = useState([]);
+  const { user } = useAuth();
+  const getAllDreamCards = () => {
+    getDreamCardsByUid(user.uid).then((scArray) => {
+      setDreamCards(scArray);
+    });
+  };
+
+  useEffect(() => {
+    getAllDreamCards();
+  }, [user.uid]);
+  console.warn(dreamCards);
+  return (
+    <div className="d-flex flex-wrap">
+      {dreamCards.map((dcObj) => (
+        <DreamCard key={dcObj.firebaseKey} dcObj={dcObj} onUpdate={getAllDreamCards} />
+      ))}
+    </div>
+  );
+}
