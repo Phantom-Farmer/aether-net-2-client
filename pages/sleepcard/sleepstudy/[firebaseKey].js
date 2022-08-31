3c7fee2c2eeab30c46876cb3ce4f62cad1c8198f
Phantom-Farmer <joeljdahl@gmail.com>
2022-08-30 22:09:57 -0500

    still can not render full sleep study

---
+++ pages/sleepcard/sleepstudy/[firebaseKey].js
@@ -0,0 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import viewThisSleepStudy from '../../../api/mergeData';
+// import SleepCard from '../../../components/SleepCard';
+// import DreamCard from '../../../components/DreamCard';
+
+export default function ViewSleepStudy() {
+  const [sleepStudy, setSleepStudy] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    viewThisSleepStudy(firebaseKey).then(setSleepStudy);
+  }, [firebaseKey]);
+
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <h2>
+        sleep study
+      </h2>
+      <h5>
+        {sleepStudy.scObj?.favorite ? ' 🤍' : ''}
+      </h5>
+      { /* <div className="sleepStudyBox">
+        <SleepCard />
+      </div>
+      <div className="sleepStudyBox">
+        <DreamCard />
+  </div> */ }
+    </div>
+  );
+}
