f1e83095e8c5545a11a75b75c97cae9ed5821279
Phantom-Farmer <joeljdahl@gmail.com>
2022-09-10 11:32:07 -0500

    music is happening

--- pages/music.js
+++ pages/music.js
@@ -1,11 +1,41 @@
-import useSound from 'use-sound';
-import { Button } from 'react-bootstrap';
-import song1 from '../public/songs/song1.mp3';
+import { useEffect, useState } from 'react';
 
-const Song1Button = () => {
-  const [play] = useSound(song1);
+function Music2() {
+  const song1 = new Audio('/songs/song1.mp3');
+  const [playInLoop, setPlayInLoop] = useState(false);
 
-  return <Button type="basic" onClick={play}>song1</Button>;
-};
+  useEffect(() => {
+    song1.load();
+  }, []);
 
-export default Song1Button;
+  useEffect(() => {
+    song1.loop = playInLoop;
+  }, [playInLoop]);
+
+  const playSound = () => {
+    song1.play();
+  };
+
+  const pauseSound = () => {
+    song1.pause();
+  };
+
+  const stopSound = () => {
+    song1.pause();
+    song1.currentTime = 0;
+  };
+
+  return (
+    <div className="Sounds">
+      <input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound} />
+      <input type="button" className="btn btn-primary mr-2" value="Pause" onClick={pauseSound} />
+      <input type="button" className="btn btn-primary mr-2" value="Stop" onClick={stopSound} />
+
+      <label>
+        <input type="checkbox" checked={playInLoop} onChange={(e) => setPlayInLoop(e.target.checked)} /> Play in Loop
+      </label>
+    </div>
+  );
+}
+
+export default Music2;
